8e1d3857670d4e4ea96e39f4a5505ff82e8de2e2
ethicnology <ethicnology@pm.me>
2021-06-16 15:19:33 -0400

    Add paper link

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -4,7 +4,8 @@ module.exports = {
     base: '/full-bitcoin-blockchain-data-made-easy/',
     themeConfig: {
 	nav: [
-	    { text: 'Scripts', link: '/scripts/' },
+		{ text: 'Paper', link: 'https://arxiv.org/pdf/2106.08072.pdf' },
+		{ text: 'Scripts', link: '/scripts/' },
 		{ text: 'Code', link: '/code/' },
 		{ text: 'Data', link: 'http://data.complexnetworks.fr/bitcoin/' },
 		{ text: 'Mirror', link: 'http://samos.lip6.fr/bitcoin' }
